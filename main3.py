7ff6437ae34773075161b28a4e1bc1b7654213ef
Harsh Sinha <sinha.harshsep@gmail.com>
2024-02-03 12:44:10 +0530

    Add files via upload

--- main3.py
+++ main3.py
@@ -18,16 +18,19 @@ def main():
         income_statement = tickerData.financials
         cash_flow = tickerData.cashflow
 
-        # Display the data in pandas DataFrame format
-        st.write("Balance Sheet:")
-        st.dataframe(balance_sheet)
+        # Display and allow download of the data in pandas DataFrame format
+        display_and_download_data("Balance Sheet", balance_sheet)
+        display_and_download_data("Income Statement", income_statement)
+        display_and_download_data("Cash Flow Statement", cash_flow)
 
-        st.write("\nIncome Statement:")
-        st.dataframe(income_statement)
+def display_and_download_data(title, data):
+    st.write(title)
+    st.dataframe(data)
+    csv = convert_df_to_csv(data)
+    st.download_button(label=f"Download {title} as CSV",data=csv,file_name=f"{title.lower().replace(' ', '_')}.csv",mime='text/csv')
 
-        st.write("\nCash Flow Statement:")
-        st.dataframe(cash_flow)
+def convert_df_to_csv(data):
+    return data.to_csv().encode('utf-8')
 
 if __name__ == "__main__":
     main()
-
